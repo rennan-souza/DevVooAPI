1a60d5cfc051ff0acf71a6fc11bb7c6c2b0db69a
Renan <souza.rnn@gmail.com>
2022-02-03 22:39:29 -0300

    Project started

---
+++ src/main/java/com/renan/devvoo/DevVooApplication.java
@@ -0,0 +1,13 @@
+package com.renan.devvoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevVooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevVooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renan/devvoo/DevVooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.devvoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevVooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
